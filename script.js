d7e8677d6d866984f36afca6236ee7e25a8577d5
Suman Ghosh <ghoshsuman845@yahoo.com>
2023-08-04 15:45:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let counter=document.getElementById("counter");
+let incrementBtn=document.getElementById("incrementBtn");
+let count=0;
+function increment(){
+	alert(count);
+	count=count+1;
+	counter.innerText=count
+	
+}
+
+incrementBtn.addEventListener("click",increment);
